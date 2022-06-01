19c820928400ccb27dbb93dadc8c2d76c50d2102
Mark Viti <+>
2022-06-01 12:08:21 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+require_relative "post.rb"
+
+class Author
+    attr_accessor :name 
+
+
+    def initialize(name)
+        @name = name 
+        @posts = []
+    end 
+
+    def add_post(post)
+        post.author= self
+    end 
+
+    def add_post_by_title(name)
+        post = Post.new(name)
+        post.author = self
+    end 
+
+    def posts
+        Post.all.select {|post| post.author == self}
+    end
+
+    def self.post_count
+        Post.all.count 
+    end
+end
